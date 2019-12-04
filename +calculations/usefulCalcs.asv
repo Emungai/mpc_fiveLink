adfe75a1a3cb743880d511da3c32498e5db1b90c
grantgib <41451443+grantgib@users.noreply.github.com>
2019-12-03 20:05:38 -0500

    deleting folders

--- +calculations/usefulCalcs.asv
+++
@@ -1,3 +0,0 @@
-
-k=1;
-(gait(k).states.x(1,end)-gait(k).states.x(1,1))/(gait(k).tspan(end)-gait(k).tspan(1))
--- +utilities/getRootPath.m
+++
@@ -1,5 +0,0 @@
-function [rootPath] = getRootPath()
-    pathstr = mfilename('fullpath');
-    [pathstr, ~, ~] = fileparts([pathstr, '.m']);
-    [rootPath, ~, ~] = fileparts(pathstr);
-end
